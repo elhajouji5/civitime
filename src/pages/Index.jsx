59d0d14f6dc7d758dd710eb06e055a910eceee0e
Elhajouji5 <=>
2021-11-05 19:40:04 +0100

    Finish male preferences

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,7 +1,52 @@
-import React from 'react'
-import Assets from '../assets/Assets'
+import React, { useState } from 'react'
+import { People, dressColors, skinColors } from '../assets/Assets'
 
 const Index = () => {
+    const [skinColor, setSkin] = useState('#FCD7B8')
+    const [dressColor, setDress] = useState(null)
+    // Indicate whether we're going to eit the male or female
+    const [gender, setGender] = useState('male')
+    // Holding the male profile preferences
+    const [maleState, setMaleState] = useState({
+        baseColor: '#B2C7C7',
+        skinColor: '#FCD7B8',
+        dressColor: null,
+        dressBackGroundColor: '#B2C7C7',
+    })
+
+    const submitForm = (e) => {
+        e.preventDefault()
+        console.log('Submitting the form..')
+    }
+
+    /**
+     * 
+     * @param {string} target targeted area
+     * @param {string} color
+     * @param {string} component The corresponding color component
+     */
+    const updateProfile = (target = 'skinColor', color = 'FCD7B8', component = null) => {
+        // User have to select the gender first
+        if (!gender) {
+            alert('Vous devez selectionner le sexe tout d\'abord')
+            return
+        }
+        color = color.replace('color', '')
+        let dressBackGroundColor = maleState.dressBackGroundColor
+        if (!(new RegExp(/color\-striped/)).test(component) && target === 'dressColor') {
+            dressBackGroundColor = `#${color}`
+        } else if (target === 'dressColor') {
+            dressBackGroundColor = maleState.baseColor
+        }
+        // Change male preferences
+        if (gender === 'male') {
+            setMaleState({
+                ...maleState,
+                [target]: `#${color}`,
+                dressBackGroundColor
+            })
+        }
+    }
 
     return (
         <div className="container center">
@@ -10,40 +55,46 @@ const Index = () => {
                 <strong>Profil</strong>
             </p>
 
-            <div className="profile-form ">
+            <form className="profile-form ">
                 {/* Form role */}
                 <p className="form-role">Lequel de ces deux avatars préférez-vous ?</p>
 
                 {/* Avatars container */}
                 <div className="flex full-size center">
                     <div className="avatar">
-                        <img src={Assets.man} className="full-size" />
+                        <People.Male className="full-size" maleState={maleState} />
                     </div>
                     <div className="avatar">
-                        <img src={Assets.woman} className="full-size" />
+                        {/* <People.Female className="full-size" /> */}
                     </div>
                 </div>
 
                 {/* Colors */}
                 <div className="icons">
+                    {/* Skin */}
                     <div className="flex">
-                        <img src={Assets.Skin} />
-                        <img src={Assets.colorFCD7B8} />
-                        <img src={Assets.colorE0A39A} />
-                        <img src={Assets.colorFCCC84} />
-                        <img src={Assets.color533724} />
+                        {
+                            Object.keys(skinColors).map(key =>
+                                (key.toLocaleLowerCase() === 'skin')
+                                    ? <img key={key} src={skinColors[key]} alt="" />
+                                    : <img style={{ cursor: 'pointer' }} key={key} onClick={() => updateProfile('skinColor', key)} src={skinColors[key]} alt={key} />
+                            )
+                        }
                     </div>
+                    {/* Dress */}
                     <div className="flex">
-                        <img src={Assets.TShirt} />
-                        <img src={Assets.colorB2C7C7} />
-                        <img src={Assets.color88C10F} />
-                        <img src={Assets.colorFF1414} />
-                        <img src={Assets.colorStriped} />
+                        {
+                            Object.keys(dressColors).map(key =>
+                                (key.toLocaleLowerCase() === 'tshirt')
+                                    ? <img key={key} src={dressColors[key]} alt="" />
+                                    : <img style={{ cursor: 'pointer' }} key={key} onClick={() => updateProfile('dressColor', key, dressColors[key])} src={dressColors[key]} alt={key} />
+                            )
+                        }
                     </div>
                 </div>
 
-                <button className="form-confirm">Valider</button>
-            </div>
+                <button type="submit" className="form-confirm" onClick={(e) => { submitForm(e) }}>Valider</button>
+            </form>
         </div>
     )
 }
